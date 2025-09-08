9627d3e6d3d952b50b88a279945119b20d5d1570
ersmax <e.masarin@student.vu.nl>
2025-09-08 17:49:31 +0200

    Setup repo

---
+++ 1_programming_set_1/1_Volume/1_volume.cpp
@@ -0,0 +1,5 @@
+/*
+*One gallon is 3785.41 milliliters. Write a program that will read the volume of
+a package of milk in milliliters, and output the volume in gallons, as well as the
+number of packets needed to yield one gallon of milk.
+*/
