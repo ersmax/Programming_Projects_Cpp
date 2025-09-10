c5818ce0213e07357f72fa0f2b0be2f7724ca7e9
ersmax <e.masarin@student.vu.nl>
2025-09-10 16:21:16 +0200

    Add plane distance

--- 1_programming_set_1/10_Plance_distance/10_Plane_distance.cpp
+++ 1_programming_set_1/10_Plance_distance/10_Plane_distance.cpp
@@ -1,6 +1,23 @@
-/*
-Write a program that reads the speed of an airplane in miles per hour and the travel
-time in hours. The program should display the distance traveled by the airplane in
-that time in miles, and its speed in meters per second. Use the equation
-Speed in m/s = (Speed in MPH/3600) X 1609.344
-*/
+#include <iostream>
+using namespace std;
+
+int main ( ) {
+    float speed_mph, time_hours;
+
+    cout.setf(ios::fixed);
+    cout.setf(ios::showpoint);
+    cout.precision(2);
+
+    cout << "Enter the speed of the airplane in miles per hour:";
+    cin >> speed_mph;
+    cout << "Enter the travel time in hours:";
+    cin >> time_hours;
+
+    float distance_miles = speed_mph * time_hours;
+    float speed_mps = (speed_mph / 3600.0f) * 1609.344f;
+
+    cout << "Distance traveled: " << distance_miles << " miles.\n"
+         << "Speed in meters per second: " << speed_mps << " miles/second.\n";
+
+    return 0;
+}
