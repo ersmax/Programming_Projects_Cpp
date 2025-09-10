e547b549158bc61366aadda0674be40b93c12c3e
ersmax <e.masarin@student.vu.nl>
2025-09-10 16:08:28 +0200

    Refactor 1.9 description

---
+++ 1_programming_set_1/10_Plance_distance/10_Plane_distance.cpp
@@ -0,0 +1,6 @@
+/*
+Write a program that reads the speed of an airplane in miles per hour and the travel
+time in hours. The program should display the distance traveled by the airplane in
+that time in miles, and its speed in meters per second. Use the equation
+Speed in m/s = (Speed in MPH/3600) X 1609.344
+*/
--- 1_programming_set_1/9_arcade_prize/9_Arcade_prize.cpp
+++ 1_programming_set_1/9_arcade_prize/9_Arcade_prize.cpp
@@ -1,11 +1,3 @@
-/*
-The video game machines at your local arcade output coupons depending on how
-well you play the game. You can redeem 10 coupons for a candy bar or 3 coupons
-for a gumball. You prefer candy bars to gumballs. Write a program that inputs the
-number of coupons you win and outputs how many candy bars and gumballs you
-can get if you spend all of your coupons on candy bars first and any remaining
-coupons on gumballs.
-*/
 #include <iostream>
 using namespace std;
 
