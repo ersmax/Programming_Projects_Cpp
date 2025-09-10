7ae0314eae5cae162258548665703d8d9a76f63a
ersmax <e.masarin@student.vu.nl>
2025-09-10 16:03:43 +0200

    Add 1.9 candy bar

---
+++ 1_programming_set_1/9_arcade_prize/9_Arcade_prize.cpp
@@ -0,0 +1,27 @@
+/*
+The video game machines at your local arcade output coupons depending on how
+well you play the game. You can redeem 10 coupons for a candy bar or 3 coupons
+for a gumball. You prefer candy bars to gumballs. Write a program that inputs the
+number of coupons you win and outputs how many candy bars and gumballs you
+can get if you spend all of your coupons on candy bars first and any remaining
+coupons on gumballs.
+*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int total_coupons;
+    int candy_bars = 0, gumballs = 0, remaining_coupons = 0;
+
+    cout << "Enter the number of coupons you have: ";
+    cin >> total_coupons;
+
+    candy_bars = total_coupons / 10;
+    remaining_coupons = total_coupons % 10;
+    gumballs = remaining_coupons / 3;
+
+    cout << "You can get " << candy_bars << " candy bar(s) and "
+         << gumballs << " gumball(s)." << endl;
+
+    return 0;
+}
