bcf8678c6775ee2b77b93e0f30e192986f69d455
ersmax <e.masarin@student.vu.nl>
2025-09-13 20:42:36 +0200

    Refactor 2.3

--- 2_programming_set_2/2.3_Financing_education/3_Financing_education.cpp
+++ 2_programming_set_2/2.3_Financing_education/3_Financing_education.cpp
@@ -1,16 +1,3 @@
-/*
-An organization intends to get people to sponsor the education of underprivileged
-children. As part of their program, they launch a website where people can register
-by entering their details and the amount (in USD) they wish to sponsor each
-month. The website calculates the number of children the donor can sponsor based
-on the amount required to educate one child.
-Write a program that inputs the amount required for one child’s education, and
-the amount a person is willing to sponsor, and calculates how many children
-can be sponsored by that person. The program should also print how much of
-the person’s amount is remaining after calculating the number of children, and
-suggest how many more dollars would educate another child. (Hint: Use a do
-while loop)
-*/
 #include <iostream>
 using namespace std;
 
