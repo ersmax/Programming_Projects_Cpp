fb48f3e1c2bb7b61e89a50bdde16ccd271e6804c
ersmax <e.masarin@student.vu.nl>
2025-10-14 22:20:08 +0200

    Add height estimator

---
+++ 3_programming_set_3/3.10_Estimated_height/10_Estimated_height.cpp
@@ -0,0 +1,60 @@
+/*
+One way to estimate the height of a child is to use the following formula, which
+uses the height of the parents:
+Hmale_child = ((Hmother 13/12) + Hfather)/2
+Hfemale_child = ((Hfather 12/13) + Hmother)/2
+All heights are in inches. Write a function that takes as input parameters the gender
+of the child, height of the mother in inches, and height of the father in inches,
+and outputs the estimated height of the child in inches. Embed your function in a
+program that allows you to test the function over and over again until telling the
+program to exit. The user should be able to input the heights in feet and inches,
+and the program should output the estimated height of the child in feet and inches.
+Use the integer data type to store the heights.
+*/
+#include <iostream>
+using namespace std;
+
+double estimatedHeight(char gender, int motherHeight, int fatherHeight);
+
+int main( ) {
+    char answer, gender;
+    int feetFather, inchesFather, feetMother, inchesMother;
+    int feetChild, inchesChild;
+
+    do {
+        cout << "Enter the height on the father in feet and inches (separated by space):";
+        cin >> feetFather >> inchesFather;
+        cout << "Enter the height on the mother in feet and inches (separated by space):";
+        cin >> feetMother >> inchesMother;
+        cout << "Enter the gender of the child (M/F):";
+        cin >> gender;
+
+        double result = estimatedHeight(gender,
+                                        feetMother * 12 + inchesMother,
+                                        feetFather * 12 + inchesFather);
+
+        feetChild = static_cast<int>(result/12);
+        inchesChild = static_cast<int>(result) % 12;
+
+        cout << "Height of the child will be: "
+             << feetChild << " feet and "
+             << inchesChild << " inches.\n";
+        cout << "Continue (y/Y)?";
+        cin >> answer;
+    } while (answer == 'y' && answer == 'Y');
+
+    cout << endl;
+    return 0;
+}
+
+double estimatedHeight(char gender, int motherHeight, int fatherHeight) {
+    double hChild;
+
+    if (gender == 'M')
+        hChild = ((motherHeight * 13/12.0) + fatherHeight) / 2;
+    else
+        hChild = ((fatherHeight * 12/13.0) + motherHeight) / 2;
+
+    return hChild;
+}
+
---
+++ 3_programming_set_3/3.10_Estimated_height/10_documentation_Estimated_height.md
@@ -0,0 +1,11 @@
+One way to estimate the height of a child is to use the following formula, which
+uses the height of the parents:
+Hmale_child = ((Hmother 13>12) + Hfather)>2
+Hfemale_child = ((Hfather 12>13) + Hmother)>2
+All heights are in inches. Write a function that takes as input parameters the gender
+of the child, height of the mother in inches, and height of the father in inches,
+and outputs the estimated height of the child in inches. Embed your function in a
+program that allows you to test the function over and over again until telling the
+program to exit. The user should be able to input the heights in feet and inches,
+and the program should output the estimated height of the child in feet and inches.
+Use the integer data type to store the heights.
