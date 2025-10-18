8eb3b73814fa1c06c324c246967c02189ddd48bd
ersmax <e.masarin@student.vu.nl>
2025-10-18 20:00:14 +0200

    Add lottery winner with bool array

---
+++ 3_programming_set_3/3.13_Price_Winners_Lottery/13_Price_Winners_Lottery_with_flag.cpp
@@ -0,0 +1,81 @@
+/*
+You have four identical prizes to give away and a pool of 25 finalists. The finalists
+are assigned numbers from 1 to 25. Write a program to randomly select the
+numbers of 4 finalists to receive a prize. Make sure not to pick the same number
+twice. For example, picking finalists 3, 15, 22, and 14 would be valid but picking
+3, 3, 31, and 17 would be invalid, because finalist number 3 is listed twice and 31
+is not a valid finalist number.
+*/
+#include <iostream>     // for cin, cout, cerr
+#include <ctime>        // for time
+#include <cstdlib>      // for rand & srand
+using namespace std;
+
+const int PARTICIPANTS = 25;
+const int PRICES = 4;
+
+void lotteryExtraction(int participants[], int winners[], bool choosen[], int poolSize, int nPrices);
+// Precondition: participants[] is an array of integers representing the participants
+//               poolSize is the number of participants in the pool
+//               choosen[] is a boolean array to track chosen participants
+//               nPrices is the number of prices to be assigned
+// Postcondition: fills winners[] with the randomly selected winners' numbers
+// Take advantage of Independence of events probability rule,
+// use a boolean array to track chosen participants.
+// This means that after selecting a winner, that participant cannot be selected again.
+
+void settingParticipants(int participants[], int pool);
+// Precondition: participants[] is an array of integers representing the participants
+//               pool is the number of participants in the pool
+// Postcondition: fills participants[] with participant numbers from 1 to tot participants.
+
+void showWinners(int winners[], int prices);
+// Precondition: winners[] is an array of integers representing the winners
+//               prices is the number of prices to be assigned
+// Postcondition: displays the number of the participants who won the prices.
+
+int main( ) {
+    int participants[PARTICIPANTS];
+    int winners[PRICES];
+    bool choosen[PARTICIPANTS] = {false};
+    int pool = PARTICIPANTS;
+    int prices = PRICES;
+
+    // seeds the C library pseudorandom generator
+    //      time(nullptr) returns current time
+    //      cast result to unsigned required by srand
+    srand((static_cast<unsigned int>(time(nullptr))));
+
+    settingParticipants(participants, pool);
+    lotteryExtraction(participants, winners, choosen, pool, prices);
+    showWinners(winners, prices);
+
+    return 0;
+}
+
+void settingParticipants(int participants[], int pool) {
+    for (int idx = 0; idx < pool; ++idx)
+        participants[idx] = idx + 1;
+}
+
+
+void lotteryExtraction(int participants[], int winners[], bool choosen[],
+                       int poolSize, int nPrices) {
+    int numberWinner;
+    int idx = 0;
+
+    // change logic to use a boolean array
+    while (idx < nPrices) {
+        numberWinner = rand( ) % poolSize;
+        if (!choosen[numberWinner]) {
+            choosen[numberWinner] = true;
+            winners[idx++] = participants[numberWinner];
+        }
+    }
+}
+
+void showWinners(int winners[], int prices) {
+    for (int idx = 0; idx < prices; ++idx)
+        cout << "Price " << idx + 1
+             << " winner is Participant n. " << winners[idx] << endl;
+}
---
+++ 3_programming_set_3/3.13_Price_Winners_Lottery/README.md
@@ -0,0 +1,9 @@
+You have four identical prizes to give away and a pool of 25 finalists. The finalists
+are assigned numbers from 1 to 25. Write a program to randomly select the
+numbers of 4 finalists to receive a prize. Make sure not to pick the same number
+twice. For example, picking finalists 3, 15, 22, and 14 would be valid but picking
+3, 3, 31, and 17 would be invalid, because finalist number 3 is listed twice and 31
+is not a valid finalist number.
+
+---
+
