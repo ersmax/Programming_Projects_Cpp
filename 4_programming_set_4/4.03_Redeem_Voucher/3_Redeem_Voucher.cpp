2c2e35562c3dd576dc6b6e7ea5c4834617b55737
ersmax <e.masarin@student.vu.nl>
2025-10-28 15:52:02 +0100

    Setting up voucher

---
+++ 4_programming_set_4/4.03_Redeem_Voucher/3_Redeem_Voucher.cpp
@@ -0,0 +1,31 @@
+/*
+You have a voucher for a bakery, which you redeem to buy baked goods of your
+choice. The value of the voucher is fixed and the total bill of the items selected
+should not exceed the voucher value. If the voucher has any value left after the
+purchase, then it cannot be utilized again. Write a program that displays 4 items
+and their prices as a menu and then reads the value of the voucher.
+Write a function, with parameters itemCode and voucherValue passed by reference,
+that deducts the price of an item from the voucher’s value only if the item’s
+value is less that or equal to the voucher value. Use a loop to invoke the above
+function as many times as required until the value of the voucher is less than or
+equal to the cheapest item on the menu.
+Finally, display the calculated sum of the prices of the items selected, and the unutilized
+balance on the voucher after purchase.
+*/
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <utility>
+using std::vector;
+using std::cin;
+using std::cout;
+using std::string;
+using std::pair;
+
+void addVoucherValue(double& voucherValue);
+
+void addItem(vector<pair<string, double>>& items);
+
+void sortItem(vector<pair<string, int>>& items);
+
