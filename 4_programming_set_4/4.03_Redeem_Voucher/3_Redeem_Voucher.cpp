8be21741a890800a59579a30d30eabd9799eafdb
ersmax <e.masarin@student.vu.nl>
2025-10-28 16:58:28 +0100

    Add logic voucher

--- 4_programming_set_4/4.03_Redeem_Voucher/3_Redeem_Voucher.cpp
+++ 4_programming_set_4/4.03_Redeem_Voucher/3_Redeem_Voucher.cpp
@@ -17,15 +17,108 @@ balance on the voucher after purchase.
 #include <vector>
 #include <string>
 #include <utility>
+#include <limits>
+#include <cassert>
 using std::vector;
 using std::cin;
 using std::cout;
 using std::string;
 using std::pair;
+using std::numeric_limits;
+using std::streamsize;
+using std::endl;
+using std::swap;
+
+constexpr int ITEMS = 4;
 
 void addVoucherValue(double& voucherValue);
 
 void addItem(vector<pair<string, double>>& items);
 
-void sortItem(vector<pair<string, int>>& items);
+void sortItem(vector<pair<string, double>>& items);
 
+void showItem(const vector<pair<string, double>>& items);
+
+void useVoucher(vector<pair<string, double>>&items, double& voucherValue, double& total);
+
+int main( ) {
+    vector<pair<string, double>> items;
+    double voucherValue, total = 0;
+
+    addVoucherValue(voucherValue);
+    for (int idx = 0; idx < ITEMS; idx++)
+        addItem(items);
+
+    assert(items.size() == ITEMS);
+    sortItem(items);
+
+    showItem(items);
+
+    useVoucher(items, voucherValue, total);
+
+    return 0;
+}
+
+
+void addVoucherValue(double& voucherValue) {
+    while (true) {
+        cout << "Enter the value of voucher:";
+        while (!(cin >> voucherValue)) {
+            cout << "Invalid Input. Enter a number:";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        }
+        // discard extra tokens
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        if (voucherValue >= 0)
+            break;
+        cout << "Invalid amount. ";
+    }
+    cout << "Voucher value: " << voucherValue << endl;
+}
+
+void addItem(vector<pair<string, double> >& items) {
+    string name;
+    double price;
+
+    while (true) {
+        cout << "Enter the name and price of item:";
+        while (!(cin >> name >> price)) {
+            cout << "Invalid input. Enter valid name and price:";
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        }
+
+        // discard extra tokens
+        cin.ignore(numeric_limits<streamsize>::max(),'\n');
+        if (price >= 0) {
+            items.emplace_back(name, price);
+            break;
+        }
+        cout << "Invalid price.\n";
+    }
+    cout << "Item " << items.back().first << " added"
+         << " (p: " << items.back().second << ")\n";
+}
+
+void sortItem(vector<pair<string, double> >& items) {
+    for (int i = 0; i < items.size(); i++) {
+        int idxMaxValue = i;
+
+        for (int j = i + 1; j < items.size(); j++)
+            if (items[i].second > items[j].second)
+                idxMaxValue = j;
+
+        if (idxMaxValue != i)
+            swap(items[i], items[idxMaxValue]);
+    }
+}
+
+void showItem(const vector<pair<string, double>>& items) {
+    for (const pair<string, double>& item : items)
+        cout << item.first << " price: " << item.second << "\n-----\n";
+}
+
+void useVoucher(vector<pair<string, double> >& items, double& voucherValue, double& total) {
+
+}
