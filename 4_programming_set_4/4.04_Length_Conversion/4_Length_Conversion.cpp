21f20ea942e6680174b6420a39736dd9324ac8e2
ersmax <e.masarin@student.vu.nl>
2025-10-28 23:45:03 +0100

    Add length

--- 4_programming_set_4/4.04_Length_Conversion/4_Length_Conversion.cpp
+++ 4_programming_set_4/4.04_Length_Conversion/4_Length_Conversion.cpp
@@ -1,43 +1,50 @@
-/*
-Write a program that will read in a length in feet and inches and output the equivalent
-length in meters and centimeters. Use at least three functions: one for input,
-one or more for calculating, and one for output. Include a loop that lets the user
-repeat this computation for new input values until the user says he or she wants to
-end the program. There are 0.3048 meters in a foot, 100 centimeters in a meter,
-and 12 inches in a foot.
-*/
-// define NDEBUG
-#include <iostream>
-#include <limits>
-#include <cassert>
+// define NDEBUG        // uncomment this line todisable assertions
+#include <iostream>     // for cin, cout, endl
+#include <limits>       // for numeric_limits, streamsize
+#include <cassert>      // for assert
+#include <cmath>        // for round
 using std::cin;
 using std::cout;
 using std::endl;
 using std::numeric_limits;
 using std::streamsize;
+using std::ios;
+using std::round;
 
-void getLength(double& feetMeter, double& inchesCentimeter);
+constexpr double FOOT = 0.3048;
+constexpr double METER = 100.0;
 
-void lengthConversion(double& feetMeter, double& inchesCentimeter);
+void getLength(double& feet, double& inches);
+// Precondition: feet and inches are declared
+// Postcondition: defines the values of feet and inches
 
-void showLength(double feetMeter, double inchesCentimeter);
+void lengthConversion(double feet, double inches, int& meters, int& cm);
+// Precondition: feet is in 5-7 range, inches is in 0-11 range
+// Postcondition: converts feet and inches to meters and centimeters
 
+void showLength(int feetMeter, int inchesCentimeter);
+// Precondition: feetMeter is in 1-2 range, inchesCentimeter is in 0-99 range
+// Postcondition: displays the length in meters and centimeters
 
 int main( ) {
-    double lengthFtMt, lengthInCm;
-    char answer;
+    // Postcondition: Prompts user for length in feet and inches,
+    //                converts it to meters and centimeters,
+    //                and displays it.
+    double lengthFt, lengthIn;
+    int meters, centimeters;
+    char answer = 'n';
 
     do {
 
-        getLength(lengthFtMt, lengthInCm);
+        getLength(lengthFt, lengthIn);
 
-        // assert(lengthFtMt >= 5 && lengthFtMt < 8 &&
-        //        lengthInCm >= 0 && lengthInCm < 12);
-        // lengthConversion(lengthFtMt, lengthInCm);
-        //
-        // assert(lengthFtMt >= 1.524 && lengthFtMt < 2.4384 &&
-        //        lengthInCm >= 0 && lengthInCm < 100);
-        // showLength(lengthFtMt, lengthInCm);
+        assert(lengthFt >= 5 && lengthFt < 8 &&
+               lengthIn >= 0 && lengthIn < 12);
+        lengthConversion(lengthFt, lengthIn, meters, centimeters);
+
+        assert(meters >= 1 && meters < 3 &&
+               centimeters >= 0 && centimeters < 100);
+        showLength(meters, centimeters);
 
         cout << "Repeat (y/Y)?";
         cin >> answer;
@@ -48,14 +55,12 @@ int main( ) {
     return 0;
 }
 
-void getLength(double& feetMeter, double& inchesCentimeter) {
-
-    int feetInt, inchesInt;
+void getLength(double& feet, double& inches) {
 
     while (true) {
         cout << "Enter the height of an adult (feet: 5-7; inches: 0-11)\n"
-             << "format: feet inches (e.g. 5 10)...: ";
-        if (!(cin >> feetInt >> inchesInt)) {
+             << "format: feet inches (e.g. 5 10)...:";
+        if (!(cin >> feet >> inches)) {
             cout << "Wrong input.\n";
             cin.clear();
             cin.ignore(numeric_limits<streamsize>::max(),'\n');
@@ -64,14 +69,30 @@ void getLength(double& feetMeter, double& inchesCentimeter) {
         // discard extra token on the same line
         cin.ignore(numeric_limits<streamsize>::max(),'\n');
 
-        if (feetInt < 5 || feetInt > 7 ||
-            inchesInt < 0 || inchesInt > 11)
+        if (feet < 5 || feet > 7 ||
+            inches < 0 || inches > 11)
             cout << "Invalid range\n";
-        else {
-            feetMeter = static_cast<double>(feetInt);
-            inchesCentimeter = static_cast<double>(inchesInt);
+        else
             break;
-        }
     }
+}
+
+void lengthConversion(double feet, double inches, int& meters, int& cm) {
+    double cent;
+    double totalMeters = feet * FOOT + (inches * (FOOT / 12.0) );
+
+    meters = static_cast<int>(totalMeters);
+    cent = (totalMeters - meters) * METER;
+    cm = static_cast<int>(round(cent + 1e-9));  // prevent rounding issues
+
+    if (cm >= 100) {
+        cm -= 100;
+        ++meters;
+    }
+}
+
+void showLength(int feetMeter, int inchesCentimeter) {
+    cout << "This is equivalent to " << feetMeter << " meters, "
+         << inchesCentimeter << " cm.\n";
 }
 
