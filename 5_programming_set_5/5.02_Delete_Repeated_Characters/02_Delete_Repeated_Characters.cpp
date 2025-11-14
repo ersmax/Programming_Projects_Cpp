472b1c75f2b8e58cc69ce21361270af9d3cec81a
ersmax <e.masarin@student.vu.nl>
2025-11-14 23:52:01 +0100

    Removing duplicates

--- 5_programming_set_5/5.02_Delete_Repeated_Characters/02_Delete_Repeated_Characters.cpp
+++ 5_programming_set_5/5.02_Delete_Repeated_Characters/02_Delete_Repeated_Characters.cpp
@@ -1,43 +1,30 @@
-/*
-Write a function called deleteRepeats that has a partially filled array of characters
-as a formal parameter and that deletes all repeated letters from the array. Since a
-partially filled array requires two arguments, the function will actually have two
-formal parameters: an array parameter and a formal parameter of type int that
-gives the number of array positions used. When a letter is deleted, the remaining
-letters are moved forward to fill in the gap. This will create empty positions at
-the end of the array so that less of the array is used. Since the formal parameter is
-a partially filled array, a second formal parameter of type int will tell how many
-array positions are filled. This second formal parameter will be a call-by-reference
-parameter and will be changed to show how much of the array is used after the
-repeated letters are deleted. For example, consider the following code:
-char a[10];
-a[0] = 'a';
-a[1] = 'b';
-a[2] = 'a';
-a[3] = 'c';
-int size = 4;
-deleteRepeats(a, size);
-After this code is executed, the value of a[0] is 'a', the value of a[1] is 'b', the
-value of a[2] is 'c', and the value of size is 3. (The value of a[3] is no longer
-of any concern, since the partially filled array no longer uses this indexed variable.)
-You may assume that the partially filled array contains only lowercase letters. Embed
-your function in a suitable test program.
-*/
-
 #include <iostream>
 #include <limits>
 #include <span>
+#include <string>
+#include <cctype>
 
 constexpr int SIZE = 10;
 
 void fillArray(char letters[], std::size_t size, int& numberUsed);
+//   Precondition: size is the declared size of the array `letters`
+//   Postcondition: numberUsed is the n of values stored in `letters`.
+// letters[0] through letters[numberUsed - 1] have been filled with
+// letters read from the keyboard.
 
 void removeDuplicates(char letters[], std::size_t size, int& numberUsed);
+//   Precondition: numberUsed is <= declared size of `letters`
+// Also, letters[0] through letters[numberUsed - 1] have values.
+//   Postcondition: all duplicate letters have been removed from
+// the array `letters`. numberUsed is updated to reflect the new
+// number of unique letters in the array.
 
 int main( ) {
     int numberUsed = 0;
     char letters[SIZE];
     fillArray(letters, std::size(letters), numberUsed);
+    removeDuplicates(letters, std::size(letters), numberUsed);
+
     for (const char element : std::span(letters, numberUsed))
         std::cout << element << " ";
 
@@ -45,7 +32,7 @@ int main( ) {
     return 0;
 }
 
-void fillArray(char letters[], std::size_t size, int& numberUsed) {
+void fillArray(char letters[], const std::size_t size, int& numberUsed) {
     std::size_t idx = 0;
     std::string next;
     std::cout << "Enter up to " << size << " letters (-1 to end).\n";
@@ -65,6 +52,26 @@ void fillArray(char letters[], std::size_t size, int& numberUsed) {
         }
 
         letters[idx++] = next[0];
-        ++numberUsed;
+    }
+    numberUsed = idx;
+}
+
+void removeDuplicates(char letters[], const std::size_t size, int& numberUsed) {
+    if (numberUsed == 0) return;
+
+    for (int p1 = 0; p1 < numberUsed; ++p1) {
+        int p2 = p1 + 1;
+        int p3 = p1 + 1;
+
+        while (p3 < numberUsed) {
+            if (letters[p1] != letters[p3]) {
+                if (p1 != p2)
+                    std::swap(letters[p2], letters[p3]);
+                ++p2;
+                ++p3;
+            } else
+                ++p3;
+        }
+        numberUsed = p2;  // trunk the `subpart` of repeated chars e.g. a b c `a` `a` `a`
     }
 }
