29b84a4c87472813e8d321a34adbfd42508f6c91
ersmax <e.masarin@student.vu.nl>
2025-11-15 20:43:11 +0100

    Remove comments in program

--- 5_programming_set_5/5.06_Contest_Score/06_Contest_Score.cpp
+++ 5_programming_set_5/5.06_Contest_Score/06_Contest_Score.cpp
@@ -1,14 +1,3 @@
-/*
-On a popular television dance show, each contestant is given scores based on 4
-parameters: technicality, expression, stage utilization, and the energy level of the
-contestant. The jury of 3 judges award scores of 1 to 10 against each parameter, with
-10 being the highest. All the scores given by a judge are summed up and averaged
-against the scores given by the other judges to obtain each contestant’s final score.
-Write a computer program that inputs scores given by each judge against all the
-parameters and calculates the overall score obtained by a contestant. The program
-should ensure that all inputs are within the allowable data ranges.
-*/
-
 // #define NDEBUG       // to disable assert statements
 #include <array>        // for std::array
 #include <iostream>     // for std::cin, std::cout
